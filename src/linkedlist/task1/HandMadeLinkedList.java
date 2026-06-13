1bb6120711e30406f7dc94606bcda68ba9ae0560
DamirKx <salimzhandamir2186@gmail.com>
2026-06-13 11:07:47 +0500

    add task 1 linkedlist

---
+++ src/linkedlist/task1/HandMadeLinkedList.java
@@ -0,0 +1,62 @@
+package linkedlist.task1;
+
+import java.util.NoSuchElementException;
+
+public class HandMadeLinkedList<T> {
+    /**
+     * Указатель на первый элемент списка. Он же first
+     */
+    private Node<T> head;
+
+    /**
+     * Указатель на последний элемент списка. Он же last
+     */
+    private Node<T> tail;
+
+    private int size = 0;
+
+    public void addFirst(T element) {
+        final Node<T> oldHead = head;
+        final Node<T> newNode = new Node<>(null, element, oldHead);
+        head = newNode;
+        if (oldHead == null) {
+            tail = newNode;
+        } else {
+            oldHead.prev = newNode;
+        }
+        size++;
+    }
+
+    public T getFirst() {
+        final Node<T> curHead = head;
+        if (curHead == null) {
+            throw new NoSuchElementException();
+        }
+        return head.data;
+    }
+
+    public void addLast(T element) {
+        final Node<T> oldTail = tail;
+        final Node<T> newNode = new Node<>(oldTail, element, null);
+        tail = newNode;
+        if (oldTail == null){
+            tail = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        size++;
+    }
+
+    public T getLast() {
+        // Реализуйте метод\
+        final Node<T> curTail = tail;
+        if (curTail == null) {
+            throw new NoSuchElementException();
+        }
+        return tail.data;
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
---
+++ src/linkedlist/task1/Practice.java
@@ -0,0 +1,19 @@
+package linkedlist.task1;
+
+public class Practice {
+    public static void main(String[] args) {
+        HandMadeLinkedList<Integer> integers = new HandMadeLinkedList<>();
+
+        integers.addFirst(1);
+        integers.addFirst(2);
+        integers.addFirst(3);
+        integers.addLast(4);
+        integers.addLast(5);
+        integers.addFirst(1);
+
+        System.out.println(integers.getFirst());
+        System.out.println(integers.size());
+        System.out.println(integers.getLast());
+        System.out.println(integers.size());
+    }
+}
